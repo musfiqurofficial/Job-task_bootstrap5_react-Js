e2cc99303e76d3cb31b6dcfef9833ef697dbec20
musfiqurofficial <musfiqurofficial1999@gmail.com>
2022-12-09 19:27:09 +0600

    login done

--- src/components/common/Login.jsx
+++ src/components/common/Login.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import { Link } from 'react-router-dom';
 import RegisterLottie from '../common/lottie/RegisterLottie';
+import google from "../../assets/google.png";
+import fb from "../../assets/fb.png";
+import Register from './Register';
 
 const Login = ({ show, handleClose }) => {
+    const [registerShow, setRegisterShow] = useState(false);
+
+    const handleRegisterClose = () => setRegisterShow(false);
+    const handleRegisterShow = () => setRegisterShow(true);
     return (
         <div>
             <Modal size="lg" show={show} onHide={handleClose}>
@@ -15,7 +22,7 @@ const Login = ({ show, handleClose }) => {
                 <Modal.Body className='m-3'>
                     <div className='row'>
                         <Form className='col-6'>
-                            <h1 className='mb-4 fw-bold'>Sing In</h1>
+                            <h1 className='mb-5 fw-bold'>Sing In</h1>
                             <Form.Group className="" controlId="exampleForm.ControlInput1">
                                 <Form.Control
                                     className='rounded-0 bg-light'
@@ -33,37 +40,22 @@ const Login = ({ show, handleClose }) => {
                                 />
                             </Form.Group>
                             <div>
-                                <button type="submit" className='btn btn-primary w-100 rounded-pill mt-4 py-2'>Sing In</button>
-                                <button className="shadow-sm group h-12 px-6 border-2 border-gray-300 rounded-lg transition duration-300 hover:border-secondary focus:bg-blue-50 active:bg-blue-100">
-                                    <div className="position-relative d-flex align-items-center space-x-4 justify-content-center">
-                                        <img src="https://tailus.io/sources/blocks/social/preview/images/google.svg" className="position-absolute left-0" alt="google logo" />
-                                        <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-secondary sm:text-base">Continue with Google</span>
-                                    </div>
-                                </button>
-                                <button className="shadow-sm group h-12 px-6 border-2 border-gray-300 rounded-lg transition duration-300 
-                                     hover:border-secondary focus:bg-blue-50 active:bg-blue-100">
-                                    <div className="position-relative d-flex align-items-center space-x-4 justify-content-center">
-                                        <img src="https://upload.wikimedia.org/wikipedia/en/0/04/Facebook_f_logo_%282021%29.svg" className="position-absolute left-0" alt="Facebook logo" />
-                                        <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-secondary sm:text-base">Continue with Facebook</span>
-                                    </div>
-                                </button>
+                                <button type="submit" className='btn btn-primary w-100 rounded-pill my-4 py-2'>Sing In</button>
+                                <button className='btn btn-light border w-100 rounded-0 py-2 mb-2'><img className='login-icon' src={fb} alt="" />  Sign in with Facebook</button>
+                                <button className='btn btn-light border w-100 rounded-0 py-2 '><img className='login-icon' src={google} alt="" /> Sign in with Google</button>
                             </div>
                         </Form>
                         <div className='col p-3'>
-                            <small>Don't have an account yet? <Link>Create new for free!</Link></small>
-                            <RegisterLottie />
+                            <small>Don't have an account yet?</small>
+                            <Link onClick={handleRegisterShow}>Create new for free!</Link>
+                            <div className='p-3'>
+                                <RegisterLottie />
+                            </div>
                         </div>
                     </div>
                 </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
             </Modal>
+            <Register registerShow={registerShow} handleRegisterClose={handleRegisterClose}></Register>
         </div>
     );
 };
--- src/components/common/Register.jsx
+++ src/components/common/Register.jsx
@@ -1,11 +1,57 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import { Link } from 'react-router-dom';
+import RegisterLottie from '../common/lottie/RegisterLottie';
+import google from "../../assets/google.png";
+import fb from "../../assets/fb.png";
 
-const Register = () => {
+const Register = ({ registerShow, handleRegisterClose }) => {
     return (
-        <div>
-            
-        </div>
+        <div >
+            <Modal size="lg" registerShow={registerShow} onHide={handleRegisterClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title className='fs-6 text-center'>Let's learn, share & inspire each other with our passion for computer engineering. Sign up now 🤘🏼</Modal.Title>
+                </Modal.Header>
+                <Modal.Body className='m-3'>
+                    <div className='row'>
+                        <Form className='col-6'>
+                            <h1 className='mb-5 fw-bold'>Create Account</h1>
+                            <Form.Group className="" controlId="exampleForm.ControlInput1">
+                                <Form.Control
+                                    className='rounded-0 bg-light'
+                                    type="email"
+                                    placeholder="Email"
+                                    autoFocus
+                                />
+                            </Form.Group>
+                            <Form.Group className="" controlId="exampleForm.ControlInput1">
+                                <Form.Control
+                                    className='rounded-0 bg-light'
+                                    type="password"
+                                    placeholder="Password"
+                                    autoFocus
+                                />
+                            </Form.Group>
+                            <div>
+                                <button type="submit" className='btn btn-primary w-100 rounded-pill my-4 py-2'>Sing In</button>
+                                <button className='btn btn-light border w-100 rounded-0 py-2 mb-2'><img className='login-icon' src={fb} alt="" />  Sign in with Facebook</button>
+                                <button className='btn btn-light border w-100 rounded-0 py-2 '><img className='login-icon' src={google} alt="" /> Sign in with Google</button>
+                            </div>
+                        </Form>
+                        <div className='col p-3'>
+                            <small>Don't have an account yet? <Link>Create new for free!</Link></small>
+                            <div className='p-3'>
+                                <RegisterLottie />
+                            </div>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
+        </div >
     );
 };
 
 export default Register;
+
+
